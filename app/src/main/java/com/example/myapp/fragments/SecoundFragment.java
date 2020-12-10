2c73570f45025a79c35aa6c18c7bfea0dba5d026
UtsavDabhi <codewithutsav@gmail.com>
2020-12-10 14:04:48 +0530

    Fragments in Android

---
+++ app/src/main/java/com/example/myapp/fragments/SecoundFragment.java
@@ -0,0 +1,31 @@
+package com.example.myapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapp.R;
+
+
+
+public class SecoundFragment extends Fragment {
+
+
+    public SecoundFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_secound, container, false);
+
+    }
+
+
+}
